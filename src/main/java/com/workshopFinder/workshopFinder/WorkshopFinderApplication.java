9ac3cc3dcefe6d4c1bb9f1dad155e11f30f756e1
Adrián Fernández Pérez <adrifernandezperez12@gmail.com>
2022-11-29 23:38:24 +0100

    Initial commit

---
+++ src/main/java/com/workshopFinder/workshopFinder/WorkshopFinderApplication.java
@@ -0,0 +1,13 @@
+package com.workshopFinder.workshopFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshopFinder/workshopFinder/WorkshopFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopFinder.workshopFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
